e5a55c585136bb9c6a5d667c3047b2a0c1966341
stalintouch <stalintouch1983@gmail.com>
2016-09-12 10:00:03 -0400

    add simple form and created article showpages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
-  
-  
+  before_action :find_article, only: [:show, :destroy, :edit]
+
   def index
   end
 
@@ -27,5 +27,9 @@ class ArticlesController < ApplicationController
 
     def article_params
       params.require(:article).permit(:title, :content)
+    end
+
+    def find_article
+      @article = Article.find(params[:id])
     end
 end
